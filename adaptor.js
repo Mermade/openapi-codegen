47a751d3645e2e937dc9dba2597eaa5dcc156812
Mike Ralphson <mike.ralphson@gmail.com>
2017-11-14 18:11:26 +0000

    adaptor; getting there with htmlDocs

--- adaptor.js
+++ adaptor.js
@@ -4,6 +4,8 @@ const util = require('util');
 
 const yaml = require('js-yaml');
 const deref = require('reftools/lib/dereference.js').dereference;
+const walkSchema = require('swagger2openapi/walkSchema').walkSchema;
+const wsGetState = require('swagger2openapi/walkSchema').getDefaultState;
 
 function transform(api, defaults) {
     let obj = Object.assign({},defaults);
@@ -15,9 +17,14 @@ function transform(api, defaults) {
     
     obj.projectName = api.info.title;
     obj.appVersion = api.info.version;
+    obj.version = api.info.version;
     obj.appDescription = api.info.description||'No description';
     obj.classname = api.info.title.toLowerCase().split(' ').join('_').split('-').join('_');
     obj.exportedName = obj.classname;
+    obj.infoEmail = api.info.contact ? api.info.contact.email : null;
+    obj.infoUrl = api.info.contact ? api.info.contact.url : null;
+    obj.licenseInfo = api.info.license ? api.info.license.name : null;
+    obj.licenseUrl = api.info.license ? api.info.license.url : null;
 
     api = deref(api,api);
 
@@ -39,6 +46,8 @@ function transform(api, defaults) {
                 let op = pathItem[o];
                 let operation = {};
                 operation.nickname = op.operationId;
+                operation.httpMethod = o;
+                operation.path = p;
                 operation.operationId = op.operationId;
                 operation.allParams = [];
                 operation.summary = op.summary;
@@ -59,6 +68,39 @@ function transform(api, defaults) {
                 container.operation = operation;
                 obj.operations.push(container);
             }
+        }
+    }
+
+    obj.apiInfo = {};
+    obj.apiInfo.apis = [];
+    obj.apiInfo.apis.push( { operations: obj.operations } );
+
+    obj.models = [];
+    if (api.components) {
+        for (let s in api.components.schemas) {
+            let schema = api.components.schemas[s];
+            let container = {}
+            let model = {};
+            model.name = s;
+            model.title = schema.title;
+            model.unescapedDescription = schema.description;
+            model.vars = [];
+            walkSchema(schema,{},wsGetState,function(schema,parent,state){
+                let entry = {};
+                entry.name = schema.name || schema.title;
+                if (!entry.name && state.property && (state.property.startsWith('properties') || 
+                    state.property.startsWith('additionalProperties'))) {
+                    entry.name = state.property.split('/')[1];
+                }
+                entry.type = schema.type;
+                entry.required = (parent.required && parent.required.indexOf(entry.name)>=0);
+                entry.isPrimitiveType = ((schema.type !== 'object') && (schema.type !== 'array'));
+                entry.complexType = schema.type;
+                entry.dataFormat = schema.format;
+                if (entry.name) model.vars.push(entry);
+            });
+            container.model = model;
+            obj.models.push(container);
         }
     }
 
