e2185b0b54ff03ae7526e9e41217d39850f40a8d
Mike Ralphson <mike.ralphson@gmail.com>
2017-11-14 15:51:05 +0000

    Working for example Node.js templates

--- adaptor.js
+++ adaptor.js
@@ -1,21 +1,23 @@
 'use strict';
 
+const util = require('util');
+
 const yaml = require('js-yaml');
 const deref = require('reftools/lib/dereference.js').dereference;
 
-function transform(api) {
-    let obj = {};
+function transform(api, defaults) {
+    let obj = Object.assign({},defaults);
 
     obj["swagger-yaml"] = yaml.safeDump(api, {lineWidth:-1});
     obj["swagger-json"] = JSON.stringify(api, null, 2);
     
-    obj.googleCloudFunctions = false;
     obj.implFolder = 'nodejs';
     
     obj.projectName = api.info.title;
     obj.appVersion = api.info.version;
     obj.appDescription = api.info.description||'No description';
     obj.classname = api.info.title.toLowerCase().split(' ').join('_').split('-').join('_');
+    obj.exportedName = obj.classname;
 
     api = deref(api,api);
 
@@ -37,19 +39,24 @@ function transform(api) {
                 let op = pathItem[o];
                 let operation = {};
                 operation.nickname = op.operationId;
+                operation.operationId = op.operationId;
                 operation.allParams = [];
+                operation.summary = op.summary;
+                operation.notes = op.description;
                 for (let pa in op.parameters) {
                     let param = op.parameters[pa];
                     let parameter = {};
                     parameter.paramName = param.name;
                     parameter.baseName = param.name;
-                    parameter.hasMore = (pa === op.parameters.length);
+                    parameter.dataType = param.schema.type;
+                    parameter.description = param.description;
+                    parameter.required = param.required;
+                    parameter.hasMore = (pa != op.parameters.length-1);
                     operation.allParams.push(parameter);
                 }
                 let container = {};
                 container.classname = operation.nickname;
                 container.operation = operation;
-                container.allParams = operation.allParams; // dupe
                 obj.operations.push(container);
             }
         }
--- cg.js
+++ cg.js
@@ -12,18 +12,21 @@ const renderer = require('./index.js');
 let s = fs.readFileSync('./specs/petstore3.json','utf8');
 let o = yaml.safeLoad(s, { json: true } );
 
-let model = adaptor.transform(o);
+let model = adaptor.transform(o, { implFolder: 'nodejs', serverPort: '3000' });
 
 let templates = {};
 templates.readme = fs.readFileSync('./templates/nodejs/README.mustache','utf8');
 templates.swagger = fs.readFileSync('./templates/nodejs/swagger.mustache','utf8');
 templates.package = fs.readFileSync('./templates/nodejs/package.mustache','utf8');
 templates.controller = fs.readFileSync('./templates/nodejs/controller.mustache','utf8');
+templates.writer = fs.readFileSync('./templates/nodejs/writer.mustache','utf8');
+templates.service = fs.readFileSync('./templates/nodejs/service.mustache','utf8');
+templates.indexGcf = fs.readFileSync('./templates/nodejs/index-gcf.mustache','utf8');
+templates.index = fs.readFileSync('./templates/nodejs/index.mustache','utf8');
 
 mkdirp('./out/nodejs');
 
 let readme = renderer.render(templates.readme, model);
-console.log(readme);
 fs.writeFileSync('./out/nodejs/README.md',readme,'utf8');
 
 let swagger = renderer.render(templates.swagger, model);
@@ -34,3 +37,15 @@ fs.writeFileSync('./out/nodejs/package.json',packageJson,'utf8');
 
 let controller = renderer.render(templates.controller, model);
 fs.writeFileSync('./out/nodejs/controller.js',controller,'utf8');
+
+let writer = renderer.render(templates.writer, model);
+fs.writeFileSync('./out/nodejs/writer.js',writer,'utf8');
+
+let service = renderer.render(templates.service, model);
+fs.writeFileSync('./out/nodejs/service.js',service,'utf8');
+
+let indexGcf = renderer.render(templates.indexGcf, model);
+fs.writeFileSync('./out/nodejs/index-gcf.js',indexGcf,'utf8');
+
+let index = renderer.render(templates.index, model);
+fs.writeFileSync('./out/nodejs/index.js',index,'utf8');
