3e28d6edd7ff1f6e71f6434d860f92b939f5a8e9
Mike Ralphson <mike.ralphson@gmail.com>
2017-11-16 14:55:52 +0000

    First live run of updateTemplates.sh

--- updateTemplates.sh
+++ updateTemplates.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 export MERGE_BASE=`cat MERGE_BASE`
-export SCBRANCH=3.0.0
+#export SCBRANCH=3.0.0
+export SCBRANCH=master
 echo Using swagger-codegen@$MERGE_BASE
 cd ../swagger-codegen
+git checkout $SCBRANCH
+git branch -D tmp
 git pull
 export NEWBASE=`git rev-parse HEAD`
 git checkout -b tmp $MERGE_BASE
@@ -12,7 +15,8 @@ git add modules
 git commit -m "Temporary commit"
 git rebase $SCBRANCH
 echo If ok:
+echo cd ../swagger-codegen
 echo cp -pr 'modules/swagger-codegen/src/main/resources/*' ../openapi-codegen/templates/
 echo git checkout $SCBRANCH
 echo git branch -D tmp
-echo echo $NEWBASE '>' ../openapi-codegen/MERGEBASE
+echo echo $NEWBASE '>' ../openapi-codegen/MERGE_BASE
