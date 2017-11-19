448eaa461cbc45b9f10044f7697d4d3a57b9dd78
Mike Ralphson <mike.ralphson@gmail.com>
2017-11-19 18:23:14 +0000

    fix; make scripts executable

--- bin/checkJs.sh
+++ bin/checkJs.sh
@@ -3,4 +3,8 @@ cd out/nodejs
 tsc -t ES6 -m system --allowJs --outFile _ts.js *.js
 rm -f _ts.js
 cd ../..
+cd out/Javascript-Closure-Angular
+tsc -t ES6 -m system --allowJs --outFile _ts.js *.js
+rm -f _ts.js
+cd ../..
 
