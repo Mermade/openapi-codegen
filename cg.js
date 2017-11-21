646ca6f4befe3897918a87a15ae04cb4216644f3
Mike Ralphson <mike.ralphson@gmail.com>
2017-11-21 08:36:51 +0000

    feat; support Swagger 1.2 definitions

--- cg.js
+++ cg.js
@@ -3,10 +3,13 @@
 
 const fs = require('fs');
 const url = require('url');
+const util = require('util');
 
 const yaml = require('js-yaml');
 const fetch = require('node-fetch');
+const co = require('co');
 const swagger2openapi = require('swagger2openapi');
+const s1p = require('swagger-tools');
 
 const processor = require('./index.js');
 
@@ -26,20 +29,112 @@ var argv = require('yargs')
 
 let configName = argv._[0] || 'nodejs';
 let config = require('./configs/'+configName+'.json');
+let defName = argv._[1] || './defs/petstore3.json';
+
+function convert20(obj){
+    if (argv.verbose) console.log('Converting OpenAPI 2.0 definition');
+    swagger2openapi.convertObj(obj,{patch:true,warnOnly:true,direct:true},function(err,openapi){
+        if (err) {
+            console.error(util.inspect(err));
+        }
+        else {
+            config.defaults.swagger = obj;
+            processor.main(openapi,config,configName);
+        }
+    });
+}
+
+function convert12(api){
+    if (argv.verbose) console.log('Converting Swagger 1.2 definition');
+    let options = {};
+    options.source = defName;
+    var base = options.source.split('/');
+    var filename = base.pop();
+    var extension = '';
+    if (filename.endsWith('.json')) {
+        extension = '.json';
+    }
+    else if (filename.endsWith('yaml')) {
+        extension = '.yaml';
+    }
+    else {
+        base.push(filename);
+    }
+    base = base.join('/');
+
+    //if (options.source.endsWith('.json') || options.source.endsWith('.yaml')) {
+    //    extension = '';
+    //}
+
+    var retrieve = [];
+    var apiDeclarations = [];
+    if (api.apis) {
+        for (var component of api.apis) {
+            component.path = component.path.replace('.{format}','.json');
+            var lbase = base;
+            if (component.path.startsWith('/')) {
+                let up = url.parse(base);
+                lbase = up.protocol + '//' + up.host;
+            }
+            if ((base.endsWith('/')) && (component.path.startsWith('/'))) {
+                lbase = base.substr(0,base.length-1);
+            }
+            if (component.path.indexOf('://')>=0) {
+                lbase = '';
+            }
+
+            var u = (lbase+component.path);
+            if (!u.endsWith(extension)) u += extension;
+            if (argv.verbose) console.log(u);
+            retrieve.push(fetch(u,options.fetchOptions)
+            .then(res => {
+                if (argv.verbose) console.log(res.status);
+                return res.text();
+            })
+            .then(data => {
+                apiDeclarations.push(yaml.safeLoad(data,{json:true}));
+            })
+            .catch(err => {
+                console.error(util.inspect(err));
+            }));
+        }
+    }
+
+    co(function* () {
+      // resolve multiple promises in parallel
+      var res = yield retrieve;
+      var sVersion = 'v1_2';
+      s1p.specs[sVersion].convert(api,apiDeclarations,true,function(err,converted){
+          if (err) {
+              console.error(util.inspect(err));
+          }
+          else {
+              if (converted.info && !converted.info.version) {
+                  converted.info.version = '1.0.0';
+              }
+              convert20(converted);
+          }
+      });
+    });
+}
 
 function main(s) {
     let o = yaml.safeLoad(s, { json: true } );
     if (argv.verbose) console.log('Loaded definition '+defName);
 
-    if (o.openapi) {
+    if (o && o.openapi) {
         processor.main(o,config,configName);
     }
     else {
-        swagger2openapi.convertObj(o,{patch:true,warnOnly:true,direct:true},function(err,openapi){
-            if (argv.verbose) console.log('Converting OpenAPI 2.0 definition');
-            config.defaults.swagger = o;
-            processor.main(openapi,config,configName);
-        });
+        if (o && o.swaggerVersion && o.swaggerVersion === '1.2') {
+            convert12(o);
+        }
+        else if (o && o.swagger && o.swagger === '2.0') {
+            convert20(o);
+        }
+        else {
+            console.error('Unrecognised OpenAPI/Swagger version');
+        }
     }
 }
 
@@ -50,7 +145,6 @@ if (argv.verbose) {
 if (argv.lint) config.defaults.lint = true;
 if (argv.debug) config.defaults.debug = true;
 
-let defName = argv._[1] || './defs/petstore3.json';
 let up = url.parse(defName);
 if (up.protocol && up.protocol.startsWith('http')) {
     fetch(defName)
