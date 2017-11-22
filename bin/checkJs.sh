7ceb942065033a0031b3c47af9430214899df044
Mike Ralphson <mike.ralphson@gmail.com>
2017-11-22 13:16:20 +0000

    travis; check JS and TS when building

--- bin/checkJs.sh
+++ bin/checkJs.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
+
+set -e
+
+echo nodejs
 cd out/nodejs
 tsc -t ES6 -m system --allowJs --outFile _ts.js *.js
 rm -f _ts.js
 cd ../..
+
+echo Javascript-Closure-Angular
 cd out/Javascript-Closure-Angular
+tsc -t ES6 -m system --allowJs --outFile _ts.js *.js
+rm -f _ts.js
+cd ../..
+
+echo Javascript
+cd out/Javascript/src
 tsc -t ES6 -m system --allowJs --outFile _ts.js *.js
 rm -f _ts.js
 cd ../..
--- bin/checkTs.sh
+++ bin/checkTs.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+
+set -e
+
+echo typescript-node
 cd out/typescript-node
+npm i
 npx tsc -t ES6 -m commonjs *.ts
 cd ../..
+
+echo typescript-fetch
 cd out/typescript-fetch
+npm i
 npx tsc -t ES6 -m commonjs *.ts
 cd ../..
+
 #cd out/typescript-angularjs
 #npx tsc -t ES6 -m commonjs *.ts
 
